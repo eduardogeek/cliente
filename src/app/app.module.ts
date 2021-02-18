2e79cb6dd3a2e6402cd07d37eb28e8e6043e6242
Eduardo Geek <eduardo.geek@gmail.com>
2021-02-17 23:31:04 -0300

    Criar componentes, rota e serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './cliente/formulario/formulario.component';
+import { TabelaComponent } from './cliente/tabela/tabela.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    TabelaComponent
   ],
   imports: [
     BrowserModule,
